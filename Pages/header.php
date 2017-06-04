8e4f2f2ac899812bc5de0846b777ce5691138884
Terry Brady <terrywbrady+c9@gmail.com>
2017-06-04 03:50:57 +0000

    updates

--- Pages/header.php
+++ Pages/header.php
@@ -1,12 +1,17 @@
 <html>
-    <header>
-        <!--jQuery Resources-->
-        <script	src="//ajax.googleapis.com/ajax/libs/jquery/1.7.2/jquery.min.js"></script>
-        <script src="//ajax.googleapis.com/ajax/libs/jqueryui/1.9.1/jquery-ui.min.js"></script>
-        <link rel="stylesheet" type="text/css" href="//ajax.googleapis.com/ajax/libs/jqueryui/1.10.3/themes/smoothness/jquery-ui.css"></link>
+<header>
+  <script	
+    src="//ajax.googleapis.com/ajax/libs/jquery/1.7.2/jquery.min.js">
+  </script>
+  <script 
+    src="//ajax.googleapis.com/ajax/libs/jqueryui/1.9.1/jquery-ui.min.js">
+  </script>
+  <link rel="stylesheet" type="text/css" 
+    href="//ajax.googleapis.com/ajax/libs/jqueryui/1.10.3/themes/smoothness/jquery-ui.css">
+  </link>
         
-        <!-- CSS and JS-->
-        <link rel="stylesheet" type="text/css" href="demo.css"/>
-        <script type="text/javascript" src="demo.js"></script>
-    </header>
-    <body>
+  <!-- CSS and JS-->
+  <link rel="stylesheet" type="text/css" href="demo.css"/>
+  <script type="text/javascript" src="demo.js"></script>
+</header>
+<body>
